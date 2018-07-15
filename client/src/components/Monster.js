158756367be0a9335c07aba53bc3681fae578045
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-15 16:55:28 -0400

    add mouse events for monster animation

--- client/src/components/Monster.js
+++ client/src/components/Monster.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Monster = props => {
 	return (
-		<div className="monster">
-      		<img src="/Neko-Breathing-Resize.gif" id="monster" alt="your monster" height= "220px" width="281px" />
+		<div className="monster" >
+      		<img src="/Neko-Breathing-Resize.gif" id="monster" alt="your monster" height= "220px" width="281px" onMouseOver={e => (e.currentTarget.src = "/Neko-Open.gif")} onMouseLeave={e => (e.currentTarget.src = "/Neko-Breathing-Resize.gif")} onMouseUp={e => (e.currentTarget.src = "/Neko-Breathing-Resize.gif")}/>
 		</div>
 	)
 }

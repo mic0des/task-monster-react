ad377bd7a190e7d55fbcc1bceb95fcf60be09e35
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-21 20:25:48 -0400

    make navigation its own component

--- client/src/App.js
+++ client/src/App.js
@@ -72,33 +72,14 @@ import ProgressBar from './components/ProgressBar';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TaskModule from './components/TaskModule';
+import Navigation from './components/Navigation';
 import './App.css';
-
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 
 export class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <Grid container spacing={24} alignItems="center" direction="row" justify="center">
-          <Grid item xs={4}>
-            <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
-              <h1 className="title" >Task Monster</h1>
-            </div>
-          </Grid>
-          <Grid style={{padding: "30px"}} item xs={3}>
-          </Grid>
-          <Grid style={{padding: "20px", marginTop: "30px"}} item xs={4}>
-            <Avatar alt="Jelo" src="https://avatars1.githubusercontent.com/u/4992682?s=460&v=4" className="avatar" />
-            <Button className="nav">Jelo</Button>
-            <Button variant="contained" color="primary" className="nav">New Task</Button>
-            <Button className="nav">Monsters</Button>
-            <Button className="nav">Log Out</Button>
-          </Grid>
-        </Grid>
+        <Navigation />
         <br/>
         <br/>
         <TaskModule />

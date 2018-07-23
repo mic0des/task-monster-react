b9ec381890fdc394b7266294e82448e7cf6d234b
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-22 20:25:03 -0400

    add gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 	has_one :monster
 	has_many :task_lists
+
+  def gravatar_url
+    email_address = self.email
+    gravatar = Digest::MD5::hexdigest(email_address).downcase 
+    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#64"
+    return !!url ? url : "http://www.gravatar.com/avatar/?d=identicon"
+  end
+
+  def gravatar_url_small
+    email_address = self.email
+    gravatar = Digest::MD5::hexdigest(email_address).downcase 
+    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#32"
+    return !!url ? url : "http://www.gravatar.com/avatar/?d=identicon"
+  end
+
+
 end

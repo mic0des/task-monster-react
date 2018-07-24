b2e69589a3dcecf0c545ddb5fb466fadcae6342d
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-24 17:05:09 -0400

    add username property to user db model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_one :monster
 	has_many :task_lists
 
-  def gravatar_url
-    email_address = self.email
-    gravatar = Digest::MD5::hexdigest(email_address).downcase 
-    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#64"
-    return !!url ? url : "http://www.gravatar.com/avatar/?d=identicon"
-  end
+	def gravatar_url
+  		email_address = self.email
+  		gravatar = Digest::MD5::hexdigest(email_address).downcase 
+  		url = "http://gravatar.com/avatar/#{gravatar}.png?s=#64"
+  		return !!url ? url : "http://www.gravatar.com/avatar/?d=identicon"
+	end
 
-  def gravatar_url_small
-    email_address = self.email
-    gravatar = Digest::MD5::hexdigest(email_address).downcase 
-    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#32"
-    return !!url ? url : "http://www.gravatar.com/avatar/?d=identicon"
-  end
-
+	def gravatar_url_small
+    	email_address = self.email
+    	gravatar = Digest::MD5::hexdigest(email_address).downcase 
+    	url = "http://gravatar.com/avatar/#{gravatar}.png?s=#32"
+    	return !!url ? url : "http://www.gravatar.com/avatar/?d=identicon"
+	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712201048) do
+ActiveRecord::Schema.define(version: 20180724012204) do
 
   create_table "monsters", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180712201048) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

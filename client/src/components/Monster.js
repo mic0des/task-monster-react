bd238c9da61517127afa30910c33bcd1c9334731
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-31 18:18:45 -0400

    make monster gifs bigger and load correct monster

--- client/src/components/Monster.js
+++ client/src/components/Monster.js
@@ -13,11 +13,12 @@ const styles = theme => ({
 
 const Monster = props => {
 	const { classes, taskMonster } = props;
+	const baseUrl = `/${taskMonster.nickname}-Normal.gif`
 	return (
 		<div className={classes.root} >
 			<Grid container spacing={24} alignItems="center" direction="row" justify="center">
 				<Grid item xs={2}>
-					<img src="/Neko-Breathing-Resize.gif" id="monster" alt="your monster" height= "55px" width="70.25px" onMouseOver={e => (e.currentTarget.src = "/Neko-Open.gif")} onMouseLeave={e => (e.currentTarget.src = "/Neko-Breathing-Resize.gif")} onMouseUp={e => (e.currentTarget.src = "/Neko-Breathing-Resize.gif")}/>
+					<img src={baseUrl} id="monster" alt="your monster" height= "55px" width="70.25px" onMouseOver={e => (e.currentTarget.src=`/${taskMonster.nickname}-Happy.gif`)} onMouseLeave={e => (e.currentTarget.src=`/${taskMonster.nickname}-Normal.gif`)} onMouseUp={e => (e.currentTarget.src=`/${taskMonster.nickname}-Normal.gif`)}/>
 				</Grid>
 				<Grid item xs={7}>				
 					<ul className="stats">

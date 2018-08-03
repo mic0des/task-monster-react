e6491faa3506848225bb54b91c4d8cd97a21ecfe
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-03 18:00:53 -0400

    implement routed components

--- client/src/App.js
+++ client/src/App.js
@@ -73,6 +73,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TaskModule from './components/TaskModule';
 import Navigation from './components/Navigation';
+import TaskLists from './components/TaskLists';
 import './App.css';
 import SignUpForm from './components/auth/SignUpForm';
 import SignInForm from './components/auth/SignInForm';
@@ -80,6 +81,7 @@ import TaskListForm from './components/TaskListForm';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Calendar from './components/Calendar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 var $            = require('jquery');
 
 class App extends Component {
@@ -107,23 +109,50 @@ class App extends Component {
     }      
   }
 
+  // render() {
+  //   // just defines the routes
+  //   return (
+  //     <div className="container-fluid">
+  //       <Navigation />
+  //       <br/>
+  //       <br/>
+  //       <Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
+  //       {this.state.taskLists.map((taskList, index)=> <Grid key={index} item xs={3}> <TaskModule lastSaved={taskList.last_saved} taskName={taskList.name} taskMonster={taskList.monster} deadline={taskList.deadline} taskListId={taskList.id} /> </Grid>)}
+  //       </Grid>
+  //       <SignUpForm/>
+  //       <SignInForm />
+  //       <TaskListForm taskLists={this.state.taskLists} />
+  //       <Calendar taskLists={this.state.taskLists} />
+  //     </div>
+  //   );
+  // }
+
   render() {
+    // just defines the routes
     return (
-      <div className="container-fluid">
-        <Navigation />
-        <br/>
-        <br/>
-        <Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
-        {this.state.taskLists.map((taskList, index)=> <Grid key={index} item xs={3}> <TaskModule lastSaved={taskList.last_saved} taskName={taskList.name} taskMonster={taskList.monster} deadline={taskList.deadline} taskListId={taskList.id} /> </Grid>)}
-        </Grid>
-        <SignUpForm/>
-        <SignInForm />
-        <TaskListForm taskLists={this.state.taskLists} />
-        <Calendar taskLists={this.state.taskLists} />
-      </div>
+      <Router>
+          <div>
+          <Route exact path="/tasks" render={()=>< TaskLists taskLists={this.state.taskLists} />} />
+          <Route exact path="/" component={SignInForm} />
+          <Route exact path="/signup" component={SignUpForm} />
+          <Route exact path="/newtask" render={()=>< TaskListForm taskLists={this.state.taskLists} />} />
+          <Route exact path="/calendar" render={()=>< Calendar taskLists={this.state.taskLists} />} />
+          </div>
+      </Router>
     );
   }
 }
+
+//      <Router>
+//        <div>
+//            <Navigation />
+//            <br/>
+//            <br/>
+//          <Route exact path="/" component={SignInForm} />
+//          <Route exact path="/signup" component={SignUpForm} />
+//          <Route exact path="/newtask" render={()=>< TaskListForm taskLists={this.state.taskLists} />} />
+//        </div>
+//      </Router>
 
 const mapStateToProps = state => {
   return ({
--- client/src/index.js
+++ client/src/index.js
@@ -18,10 +18,36 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignInForm from './components/auth/SignInForm';
+import SignUpForm from './components/auth/SignUpForm';
+import Navigation from './components/Navigation';
+import TaskListForm from './components/TaskListForm';
+
+// ReactDOM.render(
+// 	<Provider store={store}>
+//   		<Router>
+//   			<div>
+//   		    	<Navigation />
+//   		    	<br/>
+//   		    	<br/>
+//     			<Route exact path="/" component={SignInForm} />
+//     			<Route exact path="/signup" component={SignUpForm} />
+//     			<Route exact path="/newtask" render={()=>< TaskListForm taskLists={this.state.taskLists} />} />
+//     		</div>
+//   		</Router>
+//   	</Provider>,
+//   	document.getElementById('root')
+// );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+  	<div>
+  		<Navigation />
+  		<br/>
+        <br/>
+    	<App />
+    </div>
   </Provider>,
   document.getElementById('root')
 );

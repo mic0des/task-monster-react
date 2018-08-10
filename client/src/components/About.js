b2c8b8d8d3ac1d9fb303d3c5a4b272ca8127cd62
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-10 19:15:44 -0400

    finish About page explanation section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,18 +4,28 @@ import SignInForm from './auth/SignInForm';
 import Typography from '@material-ui/core/Typography';
 
 export default class About extends Component {
+
+	toTop(){
+		window.scroll({
+    		top: 0,
+    		behavior: "smooth"
+		});
+	}
+
 	render(){
 		return (
+			<div>
 			<Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
 					
 					<Grid item xs={6}>
-						<img style={{margin: "1.5em 16.5em 0.5em"}} src="/lucky-egg.png" height="80px" width="80px" alt=""/>
+						<img style={{margin: "1.5em 16.5em 0.5em"}} src="/Egg.gif" height="80px" width="80px" alt=""/>
 						<SignInForm />
 					</Grid>
 					<Grid item xs={6}>
 					 <h2><Typography style={{fontWeight: "700", color: "#3f51b5"}} variant="display2">Stay productive,</Typography></h2>
 					 	  <h2><Typography style={{fontWeight: "700", color: "#3f51b5"}} variant="display2">Make monster friends,</Typography></h2>
-					 		<h2><Typography style={{fontWeight: "700", color: "#3f51b5"}} variant="display2">Powered by the Ethereum blockchain!</Typography></h2>
+					 		<h2><Typography style={{fontWeight: "700", color: "#3f51b5"}} variant="display2">Collect 'em all!</Typography></h2>
+					 		<h3><Typography style={{fontWeight: "700", color: "#3f51b5"}} variant="headline">Powered by the Ξthereum blockchain. ♦</Typography></h3>
 					 		<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
 					 		<Grid item xs={6}>
 					 		<img src="/Schrodinger-Normal.gif" height="110px" width="140.50px"/>
@@ -25,7 +35,113 @@ export default class About extends Component {
 					 		</Grid>	
 					 		</Grid>
 					</Grid>
+					<br/>
+					<br/>
+					<br/>
+					<Grid style={{margin: "0 auto"}} item xs={1}>
+						<div style={{margin: "1.5em"}} className="arrow bounce">
+  							<a className="down fa fa-arrow-down fa-2x" href="#"></a>
+						</div>
+					</Grid>
+
 			</Grid>
+
+			<Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
+
+					<Grid item xs={6}>					
+					<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
+					<Grid item xs={6}>
+						<img style={{margin: "1.5em 0.5em 0em 12.5em"}} src="/blue-clipboard.png" height="300px" width="300px" alt=""/>
+					</Grid>
+					</Grid>
+					</Grid>
+					<Grid item xs={6}>
+					 		<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
+					 		<Grid item xs={8}>
+					 <h2><Typography style={{fontWeight: "700", color: "#3f51b5"}} variant="display2">Make to-do lists, and track progress!</Typography></h2>
+					 		</Grid>
+					 		<Grid item xs={4}>
+					 <img src="/blue-seed.png" style={{marginTop: "2em"}} height="50px" width="50px" alt=""/>
+					 		</Grid>	
+					 		</Grid>
+					<Grid item xs={8}>	
+					<Typography style={{color: "#3bbbfa"}} variant="subheading">Organize your tasks into multiple lists, set deadlines, then check off to-do items to track your progress in real-time!</Typography>
+					</Grid>
+
+
+					</Grid>
+
+			</Grid>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
+
+					<Grid item xs={6}>
+					 		<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
+					 		<Grid item xs={3}>
+					 		</Grid>
+					 		<Grid item xs={8}>
+					 <h2><Typography style={{fontWeight: "700", color: "#128678"}} variant="display2">Check off tasks, and watch your monsters grow!</Typography></h2>
+					 		</Grid>
+					 		<Grid item xs={1}>
+					 <img src="/halloween.png" style={{marginTop: "5.5em", marginLeft: "-5em"}} height="50px" width="50px" alt=""/>
+					 		</Grid>	
+					 		</Grid>
+
+					<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">	
+					<Grid item xs={3}>
+					</Grid>				 	
+					<Grid item xs={8}>	
+						<Typography style={{color: "#1ec17b"}} variant="subheading">With every to-do item checked off, your monsters get stronger! Don't miss deadlines to keep your monsters healthy and happy!</Typography>
+					</Grid>
+					</Grid>
+
+
+					</Grid>
+					<Grid item xs={6}>					
+					<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
+					<Grid item xs={6}>
+						<img style={{margin: "1.5em 0.5em 0em 3.5em"}} src="/checkmark.png" height="300px" width="300px" alt=""/>
+					</Grid>
+					</Grid>
+					</Grid>
+
+			</Grid>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+
+			<Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
+
+					<Grid item xs={6}>					
+					<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
+					<Grid item xs={6}>
+						<img style={{margin: "1.5em 0.5em 0em 12.5em"}} src="/calendar.png" height="300px" width="300px" alt=""/>
+					</Grid>
+					</Grid>
+					</Grid>
+					<Grid item xs={6}>
+					 		<Grid container spacing={12} alignItems="center" direction="row" justify="flex-start">
+					 		<Grid item xs={8}>
+					 <h2><Typography style={{fontWeight: "700", color: "#e8a13d"}} variant="display2">Keep track of task deadlines at a glance in Calendar!</Typography></h2>
+					 		</Grid>
+					 		<Grid item xs={4}>
+					 <img src="/fsm.png" style={{marginTop: "5em"}} height="50px" width="50px" alt=""/>
+					 		</Grid>	
+					 		</Grid>
+					<Grid item xs={8}>	
+					<Typography style={{color: "#fec536"}} variant="subheading">Using Calendar View, you can view your deadlines clearly for effective prioritizing, or export to Google Calendar!</Typography>
+					</Grid>
+					<a href="#" onClick={this.toTop} id="return-to-top"><i className="fa fa-arrow-up fa-2x"></i></a>
+
+					</Grid>
+
+			</Grid>
+
+			</div>
 		)
 	}
 }

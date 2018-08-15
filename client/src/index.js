d16f79e1792add8c401928e7ecedb30f2677b7ae
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-15 18:47:26 -0400

    delete old unnused code

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-// import SignInForm from './components/auth/SignInForm'
-// import SignOutLink from './components/auth/SignOutLink'
-// import SignUpForm from './components/auth/SignUpForm'
-// import Monster from './components/Monster'
-
-// ReactDOM.render(<Monster />, document.getElementById('root'));
-// registerServiceWorker();
-
-// =====
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -23,22 +8,6 @@ import SignInForm from './components/auth/SignInForm';
 import SignUpForm from './components/auth/SignUpForm';
 import Navigation from './components/Navigation';
 import TaskListForm from './components/TaskListForm';
-
-// ReactDOM.render(
-// 	<Provider store={store}>
-//   		<Router>
-//   			<div>
-//   		    	<Navigation />
-//   		    	<br/>
-//   		    	<br/>
-//     			<Route exact path="/" component={SignInForm} />
-//     			<Route exact path="/signup" component={SignUpForm} />
-//     			<Route exact path="/newtask" render={()=>< TaskListForm taskLists={this.state.taskLists} />} />
-//     		</div>
-//   		</Router>
-//   	</Provider>,
-//   	document.getElementById('root')
-// );
 
 ReactDOM.render(
   <Provider store={store}>

7a7b88874762de86b102190530e284f4b6cc0519
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-15 15:09:26 -0400

    delete old code

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,3 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import './App.css';
-
-// // react/components/App.js
-// var Router       = require('react-router');
-// var RouteHandler = Router.RouteHandler;
-// var $            = require('jquery');
- 
-// class App extends Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       signedIn: null
-//     }
-//   }
-
-//   componentWillMount() {
-//       $.ajax({
-//         method: "GET",
-//         url: "/auth/is_signed_in.json"
-//       })
-//       .done(function(data){
-//         this.setState({ signedIn: data.signed_in });
-//       }.bind(this));
-//   }
-
-//   getInitialState() {
-//     return { signedIn: null };
-//   }
-
-//   render() {
-//     return (
-//       <RouteHandler signedIn={this.state.signedIn} />
-//     )
-//   }
-// }
-
-// export default App;
-
-// ================
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <CssBaseline />
-//         <div className="App">
-//           <header className="App-header">
-//             <img src={logo} className="App-logo" alt="logo" />
-//             <h1 className="App-title">TaskMonster</h1>
-//           </header>
-//           <p className="App-intro">
-//             To get started, edit <code>src/App.js</code> and save to reload.
-//           </p>
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import TaskForm from './containers/TaskForm';
 import Tasks from './containers/Tasks';
@@ -119,38 +53,12 @@ class App extends Component {
     }
   }
 
-
-  // render() {
-  //   // just defines the routes
-  //   return (
-  //     <div className="container-fluid">
-  //       <Navigation />
-  //       <br/>
-  //       <br/>
-  //       <Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
-  //       {this.state.taskLists.map((taskList, index)=> <Grid key={index} item xs={3}> <TaskModule lastSaved={taskList.last_saved} taskName={taskList.name} taskMonster={taskList.monster} deadline={taskList.deadline} taskListId={taskList.id} /> </Grid>)}
-  //       </Grid>
-  //       <SignUpForm/>
-  //       <SignInForm />
-  //       <TaskListForm taskLists={this.state.taskLists} />
-  //       <Calendar taskLists={this.state.taskLists} />
-  //     </div>
-  //   );
-  // }
-
   render() {
     // just defines the routes
     return (
-      // <Router>
-      // <div>
-      // <Navigation user={this.parseJwt(localStorage.id_token).user_id}/>
-      // <About />
-      // </div>
-      // </Router>
       <Router>
           <div>
           <Navigation user={this.props.auth.isAuthenticated === true ? this.parseJwt(localStorage.id_token).user_id : 'guest'} />
-          <br/>
           <br/>
           {this.homePage()}
           <Route exact path="/signup" component={SignUpForm} />
@@ -164,17 +72,6 @@ class App extends Component {
     );
   }
 }
-
-//      <Router>
-//        <div>
-//            <Navigation />
-//            <br/>
-//            <br/>
-//          <Route exact path="/" component={SignInForm} />
-//          <Route exact path="/signup" component={SignUpForm} />
-//          <Route exact path="/newtask" render={()=>< TaskListForm taskLists={this.state.taskLists} />} />
-//        </div>
-//      </Router>
 
 const mapStateToProps = state => {
   return ({

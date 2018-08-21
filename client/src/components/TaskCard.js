2788a943320f8b26eb4c2c3003eb497c62b71d9a
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-20 21:28:10 -0400

    refactor component to use native fetch instead of jquery

--- client/src/components/TaskCard.js
+++ client/src/components/TaskCard.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-var $              = require('jquery');
 
 const styles = {
   checked: {},
@@ -26,26 +21,21 @@ class TaskCard extends React.Component {
 
   handleOnClick = function(id, event) {
     event.preventDefault();
-    $.ajax({
-      method: "DELETE",
-      url: `http://localhost:3001/tasks/${id}`,
-    }).done(function(data){
-      console.log(data)
-    })
+    return fetch(`http://localhost:3001/tasks/${id}`, {
+      method: 'delete'
+    }).then(response => {
     this.setState({
       task: ''
     });
-    this.props.removeTask(id)
+    this.props.removeTask(id)      
+    })
   }
 
   handleOnCheck = function(id, event) {
-    this.props.checkTask(id)
-    $.ajax({
-      method: "PATCH",
-      url: `http://localhost:3001/tasks/${id}`,
-    }).done(function(data){
-      console.log(data)
-    })
+    this.props.checkTask(id);
+    return fetch(`http://localhost:3001/tasks/${id}`, {
+      method: 'PATCH'
+    });
   }
 
   enabled(task){
@@ -69,27 +59,3 @@ class TaskCard extends React.Component {
 }
 
 export default withStyles(styles)(TaskCard);
-
-// const TaskCard = ({ task, removeTask }) => 
-//   <div>
-//     <div className="card card-inverse card-success card-primary mb-3 text-center">
-//       <div className="card-block">
-//         <blockquote className="card-blockquote">
-//           <p>{task.task}</p>
-//         </blockquote>
-//       </div>
-//       <div className="float-right"> 
-//         <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-//           <button 
-//             type="button" 
-//             onClick={() => removeTask(task.id)} 
-//             className="btn btn-danger"
-//           >
-//             <span aria-hidden="true">&times;</span>
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>;
-
-// export default TaskCard;

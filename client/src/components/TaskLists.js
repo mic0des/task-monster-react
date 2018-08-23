f5667406f1f9fe391a0513d703c5bb48b6e7a2a4
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-22 21:13:29 -0400

    add first task prompt

--- client/src/components/TaskLists.js
+++ client/src/components/TaskLists.js
@@ -13,11 +13,19 @@ class TaskLists extends React.Component {
   }
 
   render() {
+    if (this.props.taskLists.length === 0) {
+    return (
+      <div>
+        <img width="800px" height="80px" style={{marginTop: "3%", marginLeft: "13%"}} src="/First-Task.png" alt=""/>
+      </div>
+      )
+    } else {
     return (
         <Grid container spacing={24} alignItems="center" direction="row" justify="flex-start">
         {this.props.taskLists.map((taskList, index)=> <Grid key={index} item xs={3}> <TaskModule style={{height: "65%"}} finished={taskList.finished} lastSaved={taskList.last_saved} taskName={taskList.name} taskMonster={taskList.monster} deadline={taskList.deadline} taskListId={taskList.id} /> </Grid>)}
         </Grid>
     )
+  }
 }
 }
  

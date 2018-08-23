b1ce1e4f7ef4caa68e774d1396004316eb75b12a
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-08-22 21:24:48 -0400

    delete unnecessary imports

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SignInForm from './components/auth/SignInForm';
-import SignUpForm from './components/auth/SignUpForm';
-import Navigation from './components/Navigation';
-import TaskListForm from './components/TaskListForm';
 
 ReactDOM.render(
   <Provider store={store}>
